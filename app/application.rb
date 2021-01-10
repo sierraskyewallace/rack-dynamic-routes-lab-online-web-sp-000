e7c80fca00722f78744ca6bef2a8b6a4c81fdc2d
Sierra Wallace <sskwallace@gmail.com>
2021-01-10 00:40:53 +0000

    Done.

---
+++ app/application.rb
@@ -0,0 +1,25 @@
+class Application 
+  @@items = []
+  def call(env)
+    req = Rack::Request.new(env)
+    resp = Rack::Response.new
+    if req.path.match(/items/)
+      item = req.path.split("/items/").last
+      item_detail = @@items.find do |i| 
+        i.name == item
+      end
+      if item_detail
+        resp.write item_detail.price
+      else
+        resp.status = 400
+        resp.write "Item not found"
+      end
+
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
+
+    resp.finish
+  end
+end
